30e7981983fd09c122f2792013a588bfdec0bd62
k2k-com <nizamovevgenij@gmail.com>
2022-05-22 19:13:09 +0300

    header added

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,17 @@
 $(function () {
 
+    $('.header-grand__slider-inner').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: '<button class="slick-prev slick-arrow"><img src="../images/icon/prev.svg" alt=""></button>',
+        nextArrow: '<button class="slick-next slick-arrow"><img class="left" src="../images/icon/next.svg" alt=""></button>',
+    });
 
 
 
-    
+    var mixer = mixitup('.products__inner-box');
 
 });
-
-
